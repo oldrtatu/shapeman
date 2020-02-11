8a1788b82639c7883e8d0c0f4f2b95509b463936
Yash Malik <yashmalik23@Yashs-MacBook-Air.local>
2020-02-11 13:17:58 +0530

    Home page done

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,30 @@
-import '../styles/footer.css'
+import '../styles/footer.css';
 
 const Footer = () => (
-    <div>Footer</div>
-)
+	<div className="footer">
+		<div className="footer_services">
+			<div className="footer_services_heading">Services</div>
+			<div className="footer_services_container">
+				<div className="services_name">UI/UX Design</div>
+				<div className="services_name">IOS App development</div>
+				<div className="services_name">Android App development</div>
+				<div className="services_name">Website development</div>
+				<div className="services_name">Consultancy</div>
+				<div className="services_name">Desktop App development</div>
+			</div>
+		</div>
+		<div className="contact">
+			<div className="contact_heading">Contact</div>
+            <div className="contact_container">
+                <div className="contact_lines">hello@shapeman.co</div>
+                <div className="contact_lines">+91 9414573503</div>
+                <div className="contact_lines">Adani M2K Oyster Grande, Sector - 102</div>
+                <div className="contact_lines">Gurugram - 122505</div>
+            </div>
+		</div>
+        <div className="footer_line_break"></div>
+        <div className="copyright">Copyright @ Shapeman Technologies Private Limited - 2020</div>
+	</div>
+);
 
-export default Footer
+export default Footer;
